62dbf092a357993c2368cb743ae257d0f1126dc3
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-07-03 17:08:48 +0530

    Advance js day 3 completed

---
+++ Arrow_function/practice.js
@@ -0,0 +1,11 @@
+// const yep = () => {
+//  return 8*8
+// }
+
+// if one single return
+
+// const yep=() => 8*8
+
+// if single parameter also
+
+// const yep = hell => 8*8
---
+++ destructuring/pracice.js
@@ -0,0 +1,16 @@
+// let arr=[94,95,59,93,20];
+
+// const [a,b,c,d]=arr
+
+// console.log(a);
+
+// let obj={
+//     naw:"samay",
+//     roll:59,
+//     age:90
+// }
+
+// const {naw,roll,age} = obj;
+
+
+// console.log(naw,roll,age);
